825cc7f17b89cdd16d41299eec3b16fa151e9c0a
antoniotrkdz <antoniotrkdz@gmail.com>
2017-03-13 15:06:46 +0000

    added fs module to read html

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 var http = require('http');
 var server = http.createServer(handler);
+var fs = require('fs');
+var path = require('path');
 
 var message = 'I am so happy to be part of the Node Girls workshop!';
 
 function handler (request, response) {
-  response.writeHead(200, "Content-Type: text/html");
-  response.write(message);
-  response.end();
+  var endpoint = request.url;
+  console.log(endpoint);
+  if (endpoint.indexOf("/") !== -1) {
+    fs.readFile(path.join(__dirname,"public","index.html"), function(err, file) {
+      if (err) throw err;
+      response.writeHead(200, "Content-Type: text/html");
+      response.end(file);
+    });
+  }
+
+  var method = request.method;
+  console.log(method);
+  // response.writeHead(200, "Content-Type: text/html");
+  // response.write(message);
+  // response.end();
 }
 
 server.listen(3000, function () {
